18945f287b07e865a6d0926caff1d75a6bf5380b
tauz-hub <tauasanto101@gmail.com>
2023-03-29 15:44:00 -0300

    adding system to send database to developers

--- src/events/client/ready.event.js
+++ src/events/client/ready.event.js
@@ -1,5 +1,6 @@
 import { statusActivity } from '../../assets/statusActivity.js';
 import { setIntervalRemoveMute } from '../../services/setTimeoutMute/setTimeOutMute.js';
+import { sendDatabaseForDevelopers } from '../../services/database/sendDatabaseForDevelopers.js';
 
 export default {
   name: 'ready',
@@ -8,6 +9,8 @@ export default {
     setInterval(async () => {
       setIntervalRemoveMute(client);
     }, 1000);
+
+    sendDatabaseForDevelopers(client);
 
     statusActivity(client);
     console.log(`Logged as ${client.user.tag}`);
